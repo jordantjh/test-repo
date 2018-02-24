20c671868e73b3b8b11d35807f71b02b966b526d
Jordan Tan <jtan45@linux-a3.ews.illinois.edu>
2018-02-24 11:23:29 -0600

    reg_8

--- reg_8.sv
+++ reg_8.sv
@@ -8,6 +8,7 @@ module reg_8 (input  logic Clk, Reset, Shift_In, Load,
 	logic A_out;
 							 
 	blahblah
+second line more
     reg_4  reg_A_first_four_bits (.*, .D(D[7:4]) , .Shift_In(Shift_In), .Shift_Out(A_out), .Data_Out(A[7:4]) );
     reg_4  reg_A_last_four_bits (.*, .D(D[3:0]), .Shift_In(A_out), .Data_Out(A[3:0]) );
 					
